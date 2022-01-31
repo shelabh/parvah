5df4e7a053dcda13795a5aa5069f699422e2f07b
shelabh <68127067+shelabh@users.noreply.github.com>
2022-01-31 22:03:21 +0530

    newest

--- routes/user/signup.js
+++ routes/user/signup.js
@@ -14,33 +14,23 @@ const callback = async (req, res) => {
     const { name, phoneNumber } = req.body;
 
     //2. check if user exists (is user exxist then throw error)
-    const user = await database.user.findFirst({ where: { email: email } });
+    const user = await database.user.findFirst({
+      where: { phoneNumber },
+    });
     if (user) {
       return res.status(401).send("User already exists");
     }
-
-    //3. Bcrypt the user password
-    const saltRound = 10;
-    const salt = await bcrypt.genSalt(saltRound);
-    const bcryptPassword = await bcrypt.hash(password, salt);
 
     const data = {
       name,
       phoneNumber,
     };
-    if (email === process.env.ADMIN_EMAIL) {
-      data.role = UserRole.ADMIN;
-    }
+
     //6. enter the new user insisde our database
     const newUser = await database.user.create({
       data,
     });
-    const token = jwtGenerator(newUser.id);
-    const newerUser = await database.user.update({
-      where: { id: newUser.id },
-      data: { token },
-    });
-    res.json(newerUser);
+    res.json(newUser);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");

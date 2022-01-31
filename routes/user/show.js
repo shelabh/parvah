3afb1ba20e51f519cf2a18997bd9ad0f6bf28349
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-01 03:07:50 +0530

    bla

--- routes/user/show.js
+++ routes/user/show.js
@@ -11,11 +11,19 @@ const routeData = {
 const callback = async (req, res) => {
   try {
     //1. destructure the req.body(name, email, password)
-    const { model } = req.body;
+    const { name, phoneNumber } = req.body;
 
     //2. check if user exists (is user exxist then throw error)
+    const user = await database.user.findFirst({
+      where: { phoneNumber },
+    });
+    if (user) {
+      return res.status(401).send("User already exists");
+    }
+
     const data = {
-      model,
+      name,
+      phoneNumber,
     };
 
     //6. enter the new user insisde our database

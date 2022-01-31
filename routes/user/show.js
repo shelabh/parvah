89594c959e10d7f616cbd4578214041c3a88b363
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-01 03:03:31 +0530

    bla

---
+++ routes/user/show.js
@@ -0,0 +1,35 @@
+const jwtGenerator = require("../../utils/jwtGenerator");
+const { database } = require("../../prisma");
+
+// signup route
+
+const routeData = {
+  path: "/user/show",
+  method: "GET",
+};
+
+const callback = async (req, res) => {
+  try {
+    //1. destructure the req.body(name, email, password)
+    const { User } = req.body;
+
+    //2. check if user exists (is user exxist then throw error)
+    const data = {
+      User,
+    };
+
+    //6. enter the new user insisde our database
+    const newUser = await database.user.findMany({
+      data,
+    });
+    res.json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = {
+  callback,
+  routeData,
+};
